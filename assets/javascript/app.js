1ecbb11301df97a16e242a05571b37f9851a5103
Chris Cuomo <weatherfields@protonmail.com>
2019-10-29 18:46:11 -0400

    I don't know what's different,but I've definitely made progress since last time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,24 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAs0su1SC9BoU5hKeYHpzeHMvz6fvxQSwQ",
-    authDomain: "train-scheduler-3d51f.firebaseapp.com",
-    databaseURL: "https://train-scheduler-3d51f.firebaseio.com",
-    projectId: "train-scheduler-3d51f",
-    storageBucket: "train-scheduler-3d51f.appspot.com",
-    messagingSenderId: "685538040692",
-    appId: "1:685538040692:web:e35a0fb29a56f26bc5aa21",
-    measurementId: "G-HY99ZDF0H8"
-};
+// $(document).ready(function() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyAs0su1SC9BoU5hKeYHpzeHMvz6fvxQSwQ",
+        authDomain: "train-scheduler-3d51f.firebaseapp.com",
+        databaseURL: "https://train-scheduler-3d51f.firebaseio.com",
+        projectId: "train-scheduler-3d51f",
+        storageBucket: "train-scheduler-3d51f.appspot.com",
+        messagingSenderId: "685538040692",
+        appId: "1:685538040692:web:e35a0fb29a56f26bc5aa21"
+        // measurementId: "G-HY99ZDF0H8"
+      };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
+  
+console.log(firebase.database());
   //reference the firebase database
-let database = firebase.database();
+var database = firebase.database();
+// let trainSchedule = database.ref("/trainschedule");
 
-$(document).ready(function() {
 // click function for submit button
-$("#submit").on("click", function(event) {
+$("#add-train").on("click", function(event) {
     event.preventDefault();
 
 // variables for the input fields
@@ -26,27 +27,56 @@ let trainDestination = $("#destination-input").val().trim();
 let firstTrainTime = $("#first-train").val().trim();
 let FreqInput = $("#frequency-input").val().trim();
 
-// clears inputs
-$("#train-input").val("");
-$("#destination-input").val("");
-$("#first-train").val("");
-$("#frequency-input").val("");
-// new train info
+// new train info Object
 let trainInfo = {
     name: trainName,
     destination: trainDestination,
     firstTT: firstTrainTime,
     frequency: FreqInput
 };
-
 // push new train info to firebase, set would overwrite.
 database.ref().push(trainInfo);
-}); // end of click function
 
-database.ref().on("child_added", function(snapshot, prevChildKey){
+console.log("train name", trainInfo.name);
+console.log("train destination", trainInfo.destination);
+console.log("train first Train Time", trainInfo.firstTT);
+console.log("train frequency", trainInfo.frequency);
+
+// clears inputs
+$("#train-input").val("");
+$("#destination-input").val("");
+$("#first-train").val("");
+$("#frequency-input").val("");
+
+// database.ref().push(
+//     trainInfo);
+}); 
+// end of click function
+
+
+// i need to create a child
+// hopefully add to html and firebase
+database.ref().on("child_added", function(childSnapshot, prevChildKey){
+    
+    
+    // let trainName = childSnapshot.val().name;
+    // let trainDestination = childSnapshot.val().destination;
+    // let firstTrainTime = childSnapshot.val().firstTT;
+    // let FreqInput = childSnapshot.val().frequency;
+    
+    // 
     let trainInfo = snapshot.val().trainInfo;
-})
-}); // endo of Jquery
+    let trainName = trainInfo.name;
+    let trainDestination = trainInfo.destination;
+    let firstTrainTime = trainInfo.firstTT;
+    let FreqInput = trainInfo.frequency;
+    
+    // some sort of function involving moment.js???
+    
+    $("#tschedule-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + firstTrainTime + "</td><td>" + FreqInput + "</td></tr>");
+});
+// });
+ 
 
  // var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
     // starCountRef.on('value', function(snapshot) {
@@ -56,3 +86,5 @@ database.ref().on("child_added", function(snapshot, prevChildKey){
     // let curentTime = Dat.now();
     // let ref = firebase.database().ref('timestamp').update({
     //     [currentTime] : true
+
+    // trainTime.text(moment(nextTrain).format("HH:mm"));
